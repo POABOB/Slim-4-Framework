1a591b34f1c34d283a8a4ad2bfb07764076c25ee
POABOB <38785340+POABOB@users.noreply.github.com>
2022-07-25 02:47:39 +0800

    新增Validation單元測試

--- tests/unit/ValidationTest.php
+++ tests/unit/ValidationTest.php
@@ -1,19 +1,47 @@
 <?php
-
+use App\Utils\Validation;
 class ValidationTest extends \Codeception\Test\Unit
 {
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    private Validation $validation;
 
     protected function _before()
     {
-    }
-
-    protected function _after()
-    {
+        $this->validation = new Validation();
     }
 
     // tests
     public function testSomeFeature()
     {
-
+        $this->assertEmpty($this->validation->validate(
+            [
+              "Required" => "A",
+              "Min" => "B",
+              "Max" => "C",
+              "Number" => 1,
+              "Float" => 1.1,
+              "Alpha" => "F",
+              "Space" => "G",
+              "Email" => "a@gmail.com",
+              "Same" => "I",
+              "ID" => "A123456789"
+            ],
+            [
+              "Required" => ["required"],
+              "Min" => ["minLen" => 1],
+              "Max" => ["maxLen" => 1],
+              "Number" => ["numeric"],
+              "Float" => ["float"],
+              "Alpha" => ["alpha"],
+              "Space" => ["space"],
+              "Email" => ["email"],
+              "Same" => ['same' => "I"],
+              "ID" => ['id_number'],
+            ]
+        ));
     }
 }
